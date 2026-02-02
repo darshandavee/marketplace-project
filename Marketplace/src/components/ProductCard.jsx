de89876ac7bd679fd4f38e3dc61fa196b342e3f8
Colin Henderson <c.henderson@corecomtechacademy.co.uk>
2026-02-02 16:51:39 +0000

    ADD: ProductCard.jsx

---
+++ Marketplace/src/components/ProductCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard(props) {
+    return (
+      <div className="product-card" onClick={props.onSelect}>
+        <img src={props.image} alt={props.title} />
+        <h3>{props.title}</h3>
+        {/* <p>{props.description}</p> */}
+      </div>
+    );
+}
