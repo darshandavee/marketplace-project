6ef410b7de6445694a3c4227687238260211b4b8
BAEJAZ <zazeeqo@gmail.com>
2026-02-02 17:08:31 +0000

    product card altered

--- Marketplace/src/components/ProductCard.jsx
+++ Marketplace/src/components/ProductCard.jsx
@@ -3,6 +3,7 @@ export default function ProductCard(props) {
       <div className="product-card" onClick={props.onSelect}>
         <img src={props.image} alt={props.title} />
         <h3>{props.title}</h3>
+        
         {/* <p>{props.description}</p> */}
       </div>
     );
