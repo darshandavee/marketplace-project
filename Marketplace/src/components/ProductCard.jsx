020436ead99b875fe464b913465731308a64f1ce
BAEJAZ <zazeeqo@gmail.com>
2026-02-11 19:23:40 +0000

    catalogue styling, added animations

--- Marketplace/src/components/ProductCard.jsx
+++ Marketplace/src/components/ProductCard.jsx
@@ -3,14 +3,16 @@
 export default function ProductCard({product, onClick}) {
     return (
       <div className="product-card">
+        <div className="imageHolder">
         <img src={product.image} alt={product.title}
-        onClick={onClick} style={{ cursor: 'pointer'}}/>
+        onClick={onClick} style={{ cursor: 'pointer'}}/></div>
         <h3 className="title">{product.title}</h3>
-        <h4 className="price">{product.price}</h4>
+        <h4 className="price">ⓣ{product.price}</h4>
         <p className="description">{product.description}</p>
         {/* <NavLink to="./test.txt"><button className="addToCart">+</button></NavLink> */}
         <button className="addToCart">+</button>
-        <p className="starrating">☆☆☆</p>
+        <p className="addCartPrompt">Add to Cart</p>
+        <p className="starrating">★★★☆☆</p>
        
       </div>
     );
--- Marketplace/src/components/Productpage.jsx
+++ Marketplace/src/components/Productpage.jsx
@@ -14,7 +14,7 @@ export default function ProductPage({ products }) {
 
   return (
     <div>
-      <img src={product.image} />
+      <img src={product.image}/>
       <h2>{product.title}</h2>
       <p>{product.description}</p>
       <h3>{product.price}</h3>
