6457c98489ac44d4164c0f187e42c09ba29db423
“adamshaw90” <adamoshaw90@gmail.com>
2026-02-13 12:37:46 +0000

    Add Era cards to Time Portal and remove them from the homepage

--- Marketplace/src/components/Navbar.jsx
+++ Marketplace/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <ul>
         <li><NavLink to="/">Homepage</NavLink></li>
         <li><NavLink to="/product">Shop</NavLink></li>
-        <li>Lookbook</li>
+        <li><NavLink to ="/time-portal">Time Portal</NavLink></li>
         <li><NavLink to ="/contact-us">Contact Us</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
       </ul>
--- Marketplace/src/components/TimePortal.jsx
+++ Marketplace/src/components/TimePortal.jsx
@@ -1,1 +1,35 @@
-// Time Portal Test
+import HomePageCard from "./Homepagecard";
+
+export default function Homepage(props) {
+  const homepageItems = [
+    {
+      id: 1,
+      title: "SHOP PAST",
+      image: "/past.jpg",
+    },
+    {
+      id: 2,
+      title: "SHOP PRESENT",
+      image: "/present.jpg",
+    },
+    {
+      id: 3,
+      title: "SHOP FUTURE",
+      image: "/future.jpg",
+    },
+  ]
+
+    return (
+      <div className="app">
+        <div className="homepage-row">
+          {homepageItems.map((item) => (
+            <HomePageCard
+              key={item.id}
+              image={item.image}
+              title={item.title}
+            />
+          ))}
+        </div>
+      </div>
+    );
+}
