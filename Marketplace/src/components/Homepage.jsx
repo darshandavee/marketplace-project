9de60e2816c4e2268e7b74c4948a4ddf5dc8c027
darshandavee <darshandave@hotmail.co.uk>
2026-02-16 15:14:55 +0000

    adding homepage branch

--- Marketplace/src/components/Homepage.jsx
+++ Marketplace/src/components/Homepage.jsx
@@ -2,34 +2,8 @@ import HomePageCard from "./Homepagecard";
 
 export default function Homepage(props) {
   const homepageItems = [
-    // {
-    //   id: 1,
-    //   title: "SHOP PAST",
-    //   image: "/past.jpg",
-    // },
-    // {
-    //   id: 2,
-    //   title: "SHOP PRESENT",
-    //   image: "/present.jpg",
-    // },
-    // {
-    //   id: 3,
-    //   title: "SHOP FUTURE",
-    //   image: "/future.jpg",
-    // },
+    <div>
+    </div>
   ]
-
-    // return (
-    //   <div className="app">
-    //     <div className="homepage-row">
-    //       {homepageItems.map((item) => (
-    //         <HomePageCard
-    //           key={item.id}
-    //           image={item.image}
-    //           title={item.title}
-    //         />
-    //       ))}
-    //     </div>
-    //   </div>
-    // );
+    
 }
